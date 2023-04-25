9c0d5fda1eb925b058c014e08515c6d9b28e59cb
haztecaso <adrianlattes@disroot.org>
2023-04-25 17:29:58 +0200

    Implementando tests para algunas funciones

--- tests/test_formula.py
+++ tests/test_formula.py
@@ -1,8 +1,72 @@
 import pytest
-from liar.formula import Var
+from liar.formula import And, BinaryOperator, Const, Formula, Imp, Neg, Or, UnaryOperator, Var
+from liar.table import is_tauto
+
+@pytest.fixture(scope="function")
+def random_formula() -> Formula:
+    return Formula.random(10, 100)
 
     
 def test_var_str():
     vars = Var.generate(10, random=True)
     for v in vars:
         assert str(v) == v.value
+
+
+def test_simp_double_neg(random_formula: Formula):
+    f = random_formula.simp_double_neg
+    match f:
+        case Neg(Neg()):
+            pytest.fail(f"{f} contiene una doble negación")
+        case UnaryOperator(A):
+            test_simp_double_neg(A)
+        case BinaryOperator(A, B):
+            test_simp_double_neg(A)
+            test_simp_double_neg(B)
+        case _:
+            pass
+
+def test_push_neg(random_formula: Formula):
+    f = random_formula.push_neg
+    match f:
+        case Neg(f):
+            assert isinstance(f, Var) or isinstance(f, Const), f"{f} no es de tipo Var ni Const"
+        case UnaryOperator(A):
+            test_push_neg(A)
+        case BinaryOperator(A, B):
+            test_push_neg(A)
+            test_push_neg(B)
+        case _:
+            pass
+
+def test_subs_imp(random_formula: Formula):
+    f = random_formula.subs_imp
+    match f:
+        case Imp():
+            pytest.fail(f"{f} contiene una implicación")
+        case UnaryOperator(A):
+            test_subs_imp(A)
+        case BinaryOperator(A, B):
+            test_subs_imp(A)
+            test_subs_imp(B)
+        case _:
+            pass
+
+def test_distribute_or(random_formula: Formula):
+    f = random_formula.distribute_or
+    match f:
+        case Or(And(_,_), _):
+            pytest.fail(f"{f} contiene un And dentro de un Or")
+        case Or(_, And(_,_)):
+            pytest.fail(f"{f} contiene un And dentro de un Or")
+        case UnaryOperator(A):
+            test_distribute_or(A)
+        case BinaryOperator(A, B):
+            test_distribute_or(A)
+            test_distribute_or(B)
+        case _:
+            pass
+
+def test_is_tauto_congjuent(random_formula: Formula):
+    assert random_formula.is_tauto == is_tauto(random_formula) 
+
