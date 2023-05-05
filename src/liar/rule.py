c22275d954c0ffac4e1145d9d1b6daa0777ab6d7
haztecaso <adrianlattes@disroot.org>
2023-05-05 11:40:09 +0200

    Separando pattern_match de la clase Rule

--- src/liar/rule.py
+++ src/liar/rule.py
@@ -1,5 +1,40 @@
-from collections.abc import Iterable
+from collections.abc import Iterator
 from .formula import Formula, Var, Const, UnaryOperator, BinaryOperator
+
+def pattern_match(pattern: Formula, subject: Formula)-> Iterator[dict[Var, Formula] | None]:
+    def match_inner(
+        current_pattern: Formula, value: Formula, bindings: dict[Var, Formula]
+    ) -> bool:
+        match (current_pattern, value):
+            case (Const.TRUE, Const.TRUE):
+                return True
+            case (Const.FALSE, Const.FALSE):
+                return True
+            case (Var() as v, _):
+                if not v in bindings:
+                    bindings[v] = value
+                elif bindings[v] != value:
+                    return False
+                return True
+            case (UnaryOperator(A), UnaryOperator(B)):
+                if current_pattern.__class__ != value.__class__:
+                    return False
+                return match_inner(A, B, bindings)
+            case (BinaryOperator(A, B), BinaryOperator(C, D)):
+                if current_pattern.__class__ != value.__class__:
+                    return False
+                return match_inner(A, C, bindings) and match_inner(B, D, bindings)
+            case _:
+                return False
+
+    for subformula in subject.traverse():
+        assert isinstance(subformula, Formula)
+        bindings: dict[Var, Formula] = {}
+        if match_inner(pattern, subformula, bindings):
+            yield bindings
+        else:
+            yield None
+    
 
 
 class Rule:
@@ -10,38 +45,8 @@ class Rule:
             self.head.vars
         ), "Las variables del cuerpo de la regla deben aparecer en la cabecera"
 
-    def match(self, value: Formula) -> Iterable[dict[Var, Formula] | None]:
-        def match_inner(
-            pattern: Formula, value: Formula, bindings: dict[Var, Formula]
-        ) -> bool:
-            match (pattern, value):
-                case (Const.TRUE, Const.TRUE):
-                    return True
-                case (Const.FALSE, Const.FALSE):
-                    return True
-                case (Var() as v, _):
-                    if not v in bindings:
-                        bindings[v] = value
-                    elif bindings[v] != value:
-                        return False
-                    return True
-                case (UnaryOperator(A), UnaryOperator(B)):
-                    if pattern.__class__ != value.__class__:
-                        return False
-                    return match_inner(A, B, bindings)
-                case (BinaryOperator(A, B), BinaryOperator(C, D)):
-                    if pattern.__class__ != value.__class__:
-                        return False
-                    return match_inner(A, C, bindings) and match_inner(B, D, bindings)
-                case _:
-                    return False
-        for subformula in value.traverse():
-            assert isinstance(subformula, Formula)
-            bindings: dict[Var, Formula] = {}
-            if match_inner(self.head, subformula, bindings):
-                yield bindings
-            else:
-                yield None
+    def match(self, subject: Formula) -> Iterator[dict[Var, Formula] | None]:
+        return pattern_match(self.head, subject)
 
     def apply(self, value: Formula) -> Formula | None:
         ...
