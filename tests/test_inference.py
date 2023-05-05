ce18b5ce763b33a4ab2d19711ee5966e1084c9b3
haztecaso <adrianlattes@disroot.org>
2023-05-05 11:40:20 +0200

    Tests de InferenceRule

---
+++ tests/test_inference.py
@@ -0,0 +1,57 @@
+from liar.formula import Var
+import pytest
+from liar.inference import InferenceRule
+from liar.natural_deduction import rules
+
+def test_inference_is_sound():
+    A = Var("A")
+    for rule in rules:
+        assert rule.is_sound
+    assert InferenceRule((), A | (~A))
+
+
+def test_inference_is_sound_false():
+    A, B = Var.generate(2)
+    assert InferenceRule(A, B)
+    assert InferenceRule((), A)
+
+def test_inference_rule_specialize():
+    P = Var('P')
+    Q = Var('Q')
+    R = Var('R')
+    S = Var('S')
+    X = Var('X')
+    Y = Var('Y')
+    r1 = InferenceRule(~~P, P)
+    r2 = r1.specialize({P: Q>>R})
+    assert r2.is_specialization(r1)
+
+    r3 = r1.specialize({P: X})
+    assert r3.is_specialization(r1)
+
+    r1 = InferenceRule((Q>>P, ~P), ~Q)
+    r2 = r1.specialize({Q: ~R, P: X >> S})
+    assert r2.is_specialization(r1)
+
+    r1 = InferenceRule((P|Q)|S, Q|S)
+    r2 = r1.specialize({P: X&R, Q:~Y})
+    assert r2.is_specialization(r1)
+
+    r1 = InferenceRule((P|Q)|S, Q|S)
+    r2 = r1.specialize({P: P&Q, Q:~~Q})
+    assert r2.is_specialization(r1)
+
+def test_inference_rule_specialize_false():
+    P = Var('P')
+    Q = Var('Q')
+    R = Var('R')
+
+    r1 = InferenceRule(~~P, P)
+    r2 = InferenceRule(Q>>P, ~Q)
+    r3 = InferenceRule((Q>>P, ~P), ~Q)
+    assert r2.is_specialization(r1) == False
+    assert r3.is_specialization(r1) == False
+
+    r1 = InferenceRule((P, P>>Q), Q)
+    r2 = InferenceRule((R, P>>Q), Q)
+    assert r2.is_specialization(r1) == False
--- tests/test_rule.py
+++ tests/test_rule.py
@@ -1,19 +1,9 @@
 from liar.formula import Var
 import pytest
-from liar.rule import Rule, InferenceRule
-from liar.natural_deduction import rules
+from liar.rule import Rule
 
 
 def test_rule_match():
     pass
 
-def test_inference_is_sound():
-    A = Var('A')
-    for rule in rules:
-        assert rule.is_sound
-    assert InferenceRule((), A|(~A))
-        
-def test_inference_is_sound_false():
-    A, B = Var.generate(2)
-    assert InferenceRule(A, B)
-    assert InferenceRule((), A)
+
