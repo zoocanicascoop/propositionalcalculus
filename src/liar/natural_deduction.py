42a385d9a65cf4d9cf38fd7ebf03be3cf5d2ee45
haztecaso <adrianlattes@disroot.org>
2023-05-09 17:59:03 +0200

    Sistemas de reglas

--- src/liar/natural_deduction.py
+++ src/liar/natural_deduction.py
@@ -3,13 +3,13 @@ from .inference import InferenceRule
 
 A, B, C = Var.generate(3)
 
-elim_and_left = InferenceRule(A & B, A)
-elim_and_right = InferenceRule(A & B, B)
-modus_ponens = InferenceRule((A >> B, A), B)
-elim_double_neg = InferenceRule(~~A, A)
-intro_or_left = InferenceRule(A, A | B)
-intro_or_right = InferenceRule(A, B | A)
-intro_and = InferenceRule((A, B), A & B)
+elim_and_left = InferenceRule("E∧1", A & B, A)
+elim_and_right = InferenceRule("E∧2", A & B, B)
+modus_ponens = InferenceRule("MP", (A >> B, A), B)
+elim_double_neg = InferenceRule("E¬¬", ~~A, A)
+intro_or_left = InferenceRule("I∨1", A, A | B)
+intro_or_right = InferenceRule("I∨2", A, B | A)
+intro_and = InferenceRule("I∧", (A, B), A & B)
 
 # TODO: entender bien como codificar las reglas y cuales nos hacen falta
 
---
+++ src/liar/propositional_axioms.py
@@ -0,0 +1,151 @@
+from liar.inference import (
+    InferenceRule,
+    Proof,
+    ProofStepApplyRule,
+    ProofStepSpecializeAxiom,
+    Binding
+)
+from .formula import Formula, Var
+
+
+A, B, C = Var.generate(3)
+
+
+modus_ponens = InferenceRule("MP", (A, A >> B), B)
+
+
+def MP(*indices: int): 
+    return ProofStepApplyRule(modus_ponens, indices)
+
+def Ax(axiom_index: int, binding: Binding): 
+    return ProofStepSpecializeAxiom(axiom_index, binding)
+
+axioms: list[Formula] = [
+    ~A >> (A >> B),
+    B >> (A >> B),
+    (A >> B) >> ((~A >> B) >> B),
+    (A >> (B >> C)) >> ((A >> B) >> (A >> C)),
+    A >> (B >> (A & B)),
+    (A & B) >> A,
+    (A & B) >> B,
+    A >> (A | B),
+    B >> (A | B),
+    (A >> C) >> ((B >> C) >> ((A | B) >> C)),
+]
+
+
+a_implies_a_proof = Proof(
+    axioms,
+    A,
+    A,
+    [
+        Ax(1, {A: A >> A, B: A}),
+        Ax(3, {A: A, B: A >> A, C: A}),
+        MP(1, 2),
+        Ax(1, {A: A, B: A}),
+        MP(4, 3),
+    ],
+)
+
+
+def pad_steps(
+    steps: list[ProofStepApplyRule | ProofStepSpecializeAxiom],
+    pad_assumptions: int = 0,
+    pad_axioms: int = 0,
+) -> list[ProofStepApplyRule | ProofStepSpecializeAxiom]:
+    raise NotImplementedError()
+
+
+elim_double_neg = InferenceRule(
+    "E¬¬",
+    ~~A,
+    A,
+    Proof(
+        axioms,
+        ~~A,
+        A,
+        [
+            Ax(0, {A: ~A, B: A}),
+            MP(0, 1),
+            Ax(1, {A: ~A, B: A}),
+            # TODO: Usar sub-demostración de A >> A
+            Ax(1, {A: A >> A, B: A}),
+            Ax(3, {A: A, B: A >> A, C: A}),
+            MP(4, 5),
+            Ax(1, {A: A, B: A}),
+            MP(7, 6),
+            # final de A >> A
+            Ax(2, {A: A, B: A}),
+            MP(8, 9),
+            MP(2, 10),
+        ],
+    ),
+)
+
+
+intro_and = InferenceRule(
+    "I∧",
+    (A, B),
+    A & B,
+    Proof(
+        axioms,
+        (A, B),
+        A & B,
+        [
+            Ax(4, {A: A, B: B}),
+            MP(0, 2),
+            MP(1, 3),
+        ],
+    ),
+)
+
+intro_double_neg = InferenceRule(
+    "I¬¬",
+    A,
+    ~~A,
+    Proof(
+        axioms,
+        A,
+        ~~A,
+        [
+            Ax(2, {A: ~A, B: A >> ~~A}),
+            Ax(0, {A: A, B: ~~A}),
+            MP(2, 1),
+            Ax(1, {A: A, B: ~~A}),
+            MP(4, 3),
+            MP(0, 5),
+        ],
+    ),
+)
+
+
+intro_or_left = InferenceRule(
+    "I∧1",
+    A,
+    A | B,
+    Proof(
+        axioms,
+        A,
+        A | B,
+        [
+            Ax(7, {A: A, B: B}),
+            MP(0, 1),
+        ],
+    ),
+)
+
+
+intro_or_right = InferenceRule(
+    "I∧2",
+    A,
+    B | A,
+    Proof(
+        axioms,
+        A,
+        B | A,
+        [
+            Ax(8, {A: B, B: A}),
+            MP(0, 1),
+        ],
+    ),
+)
