e4fe8620e95dba8174fb7b92cc871e0c22891544
haztecaso <adrianlattes@disroot.org>
2023-05-18 15:17:54 +0200

    Ajustando test de inference

--- tests/test_inference.py
+++ tests/test_inference.py
@@ -8,13 +8,13 @@ def test_inference_is_sound():
     A = Var("A")
     for rule in rules:
         assert rule.is_sound
-    assert InferenceRule((), A | (~A))
+    assert InferenceRule("", (), A | (~A))
 
 
 def test_inference_is_sound_false():
     A, B = Var.generate(2)
-    assert InferenceRule(A, B)
-    assert InferenceRule((), A)
+    assert InferenceRule("", A, B)
+    assert InferenceRule("", (), A)
 
 
 def test_inference_rule_specialize():
@@ -24,22 +24,22 @@ def test_inference_rule_specialize():
     S = Var("S")
     X = Var("X")
     Y = Var("Y")
-    r1 = InferenceRule(~~P, P)
+    r1 = InferenceRule("", ~~P, P)
     r2 = r1.specialize({P: Q >> R})
     assert r2.is_specialization(r1)
 
     r3 = r1.specialize({P: X})
     assert r3.is_specialization(r1)
 
-    r1 = InferenceRule((Q >> P, ~P), ~Q)
+    r1 = InferenceRule("", (Q >> P, ~P), ~Q)
     r2 = r1.specialize({Q: ~R, P: X >> S})
     assert r2.is_specialization(r1)
 
-    r1 = InferenceRule((P | Q) | S, Q | S)
+    r1 = InferenceRule("", (P | Q) | S, Q | S)
     r2 = r1.specialize({P: X & R, Q: ~Y})
     assert r2.is_specialization(r1)
 
-    r1 = InferenceRule((P | Q) | S, Q | S)
+    r1 = InferenceRule("", (P | Q) | S, Q | S)
     r2 = r1.specialize({P: P & Q, Q: ~~Q})
     assert r2.is_specialization(r1)
 
@@ -49,12 +49,12 @@ def test_inference_rule_specialize_false():
     Q = Var("Q")
     R = Var("R")
 
-    r1 = InferenceRule(~~P, P)
-    r2 = InferenceRule(Q >> P, ~Q)
-    r3 = InferenceRule((Q >> P, ~P), ~Q)
+    r1 = InferenceRule("", ~~P, P)
+    r2 = InferenceRule("", Q >> P, ~Q)
+    r3 = InferenceRule("", (Q >> P, ~P), ~Q)
     assert r2.is_specialization(r1) == False
     assert r3.is_specialization(r1) == False
 
-    r1 = InferenceRule((P, P >> Q), Q)
-    r2 = InferenceRule((R, P >> Q), Q)
+    r1 = InferenceRule("", (P, P >> Q), Q)
+    r2 = InferenceRule("", (R, P >> Q), Q)
     assert r2.is_specialization(r1) == False
